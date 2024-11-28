40fd8877716e2b743ef00aa0de7d73110cbc1fdf
Emma Linares <elinares235@gmail.com>
2024-11-28 16:40:44 -0300

    Step One react proyect

---
+++ src/bases/01-const-var.js
@@ -0,0 +1,17 @@
+// const & var usage
+
+const name = 'Aaron';
+const lastName = 'Linares';
+
+let valorDado = 2;
+valorDado = 1;
+
+console.log( name, lastName, valorDado);
+
+if ( true ) {
+    const name = 'Emma';
+    console.log(name);
+}
+
+console.log(valorDado);
+
